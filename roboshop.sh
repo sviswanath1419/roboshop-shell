7f0a8c011b8f5c866a5a11293c33a5708640621e
sviswanath1419 <s.viswanath1419@outlook.com>
2024-04-29 15:00:50 +0530

    roboshop-shell -ec2-build

--- roboshop.sh
+++ roboshop.sh
@@ -2,11 +2,11 @@
 AMI="ami-0f3c7d07486cad139"
 SG="sg-0a082b6ef7bd6d1e4"
 INSTANCE_TYPE="t2.micro"
-INSTANCES=@{'mongodb','catalogue'}
+INSTANCES=('mongodb','catalogue')
 
-for i in $INSTANCES
+for i in "${INSTANCES[@]}"
 {
     echo "EC2 is a  ec2 $i"
 }
 
-aws ec2 run-instances --image-id AMI --count 2 --instance-type $INSTANCE_TYPE --key-name MyKeyPair --security-group-ids $SG --subnet-id subnet-xxxxxxxx
+# aws ec2 run-instances --image-id AMI --count 2 --instance-type $INSTANCE_TYPE --key-name MyKeyPair --security-group-ids $SG --subnet-id subnet-0823786c144f61f53
